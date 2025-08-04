8bf1772ec7c84d4d0f163cd3622f5d23f87e23c7
Amunr <amunreddy@gmail.com>
2025-08-04 17:37:42 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -7,8 +7,3 @@ def home():
     return render_template('homepage.html')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000) 
-
-
-@app.route('/api/dashboard')
-def dashboard():
-    
